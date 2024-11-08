d5ea492c764638c4bb9e1a76c64d53ad4ff67dbb
nikrenxd <nikrenc1101@gmail.com>
2024-11-08 19:08:55 +0200

    logger and tests

---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+pytestmark = pytest.mark.anyio
+
+
+async def test_api(client):
+    params = {"project_name": "Django"}
+    res = await client.get("/search/", params=params)
+
+    assert res.status_code == 200
+    assert len(res.json()) > 0
---
+++ tests/test_projects_service.py
@@ -0,0 +1,10 @@
+import pytest
+
+pytestmark = pytest.mark.anyio
+
+
+class TestProjectsService:
+    # TODO: Tests for service
+    async def test_search_projects(self): ...
+
+    async def test_save_project(self): ...
