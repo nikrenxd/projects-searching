4d210995275002ea7389c3aad0a3c1d6d453989b
nikrenxd <nikrenc1101@gmail.com>
2025-03-14 16:47:36 +0200

    added cors for frontend app

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.projects import project_router
 from src.core.redis import RedisClient
+from src.core.config import settings
 
 
 @asynccontextmanager
@@ -18,6 +19,8 @@ async def lifespan(app: FastAPI):
 
 def create_app() -> FastAPI:
     _app = FastAPI(lifespan=lifespan)
+
+    _app.add_middleware(CORSMiddleware, allow_origins=[settings.FE_TRUSTED_URL])
 
     _app.include_router(project_router)
 
